6e4cb4a74e7aa503ca54c0937de035276bc3c90a
Dhanenero27 <beatjamsquad27@gmail.com>
2024-03-05 16:01:09 +0800

    Register_Web

---
+++ register.js
@@ -0,0 +1,24 @@
+console.log("Hello World");
+console.error("ERROR HAPPENED!!!");
+console.warn("Warning!!")
+
+
+//variables
+//string = Words
+//Numbers = 12 12 12
+// boolean = true, false 
+//object = {"Aple", banna}
+
+//console.log(typeof(true));
+let name = 'Dan',
+lastname = 'Enero',
+age = 20,
+isProf = true;
+hobies = ["Guitar"," PLay video game"],
+run = function () {
+    console.log("Im running");
+}
+console.log(name, age, isProf, hobies);
+console.table(hobies);
+console.log("My name is " +  name + "" + lastname);
+console.log(`My name is ${name} ${lastname}`);
